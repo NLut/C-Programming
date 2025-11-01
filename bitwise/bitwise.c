765817db52c0a2bc6ff3fd0c29f0afec514c2e44
NLut <Natapat2001@gmail.com>
2025-11-01 19:32:16 +0700

    Learn about bitwise operation

---
+++ bitwise/bitwise.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "bitwise.h"
+void binprintf(int v);
+
+int main(){
+    // Shift left bit by bit
+    unsigned long x = 0;
+    printf("Shift left operation\n");
+    printf("----------------------\n");
+    for (int i = 1; i < 32; i=1<<i){
+        printf("The value of i is %d\n", i);
+    }
+    puts("");
+
+    // Shift right bit by bit
+    printf("Shift right operation\n");
+    printf("----------------------\n");
+    for (int i = 32; i > 1; i=i>>1){ // can also write as i>>=1
+        printf("The value of i is %d\n", i);
+    }
+    puts("");
+
+    // Value from bitwise.h
+    printf("Value from bitwise.h\n");
+    printf("----------------------\n");
+    printf("The value of LONG3_S2 is: %ld, %08x\n", LONG3_S2, LONG3_S2);
+    binprintf(LONG3_S2);
+    printf("The value of LONG2_S2 is: %ld, %08x\n", LONG2_S2, LONG2_S2);
+    binprintf(LONG2_S2);
+    printf("The value of LONG1_S2 is: %ld, %08x\n", LONG1_S2, LONG1_S2);
+    binprintf(LONG1_S2);
+    printf("The value of ~LONG3_S2 is: %ld, %08x\n", ~LONG3_S2, ~LONG3_S2);
+    binprintf(~LONG3_S2);
+    printf("The value of 0 =& LONG2_S2 is: %ld, %08x\n", (x &= LONG2_S2), (x &= LONG2_S2)); // Bitwise and
+    binprintf(x &= LONG2_S2);
+    printf("The value of 0 |& LONG2_S2 is: %ld, %08x\n", (x |= LONG2_S2), (x |= LONG2_S2)); // Bitwise or
+    binprintf(x |= LONG2_S2);
+
+}
+
+void binprintf(int v)
+{
+    unsigned int mask=1<<((sizeof(int)<<3)-1);
+    printf("Mask value: %d", mask);
+    while(mask) {
+        printf("%d", (v&mask ? 1 : 0)); // & = bitwise and operation -> between v and mask
+        mask >>= 1; // mask = mask shift right by 1 bit
+    }
+    puts("");
+}
---
+++ bitwise/bitwise.h
@@ -0,0 +1,3 @@
+#define LONG3_S2 (3L << 2)  // 3 * 4 -> since the initialize value is 3 and do shift left for 2 bits
+#define LONG2_S2 (2L << 2)  // 2 * 4
+#define LONG1_S2 (1L << 2)  // 1 * 4
