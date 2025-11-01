3cbe2f4d80ee9595f1905a4856851ae382cf3c69
NLut <Natapat2001@gmail.com>
2025-11-01 19:35:20 +0700

    Update arrow, previous version is error because inappropriate char[]

--- pointer/arrow.c
+++ pointer/arrow.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
-struct Person {
-    char name[50];
-    int age;
-};
-
-struct Person person1;
-struct Person *person2;
+#include <stdlib.h>
 
 int main(){
+    typedef struct {
+        // char* name;
+        int age;
+    } Person; 
+
+    Person person1;
+    Person* person2;
+    // int* person2_ptr = malloc(sizeof(Person)); // this malloc should be in main(entry point)
     person1.age = 15;
     printf("This person1 salary is %d.\n", person1.age);
     person1.age = 20;
     printf("New person1 salary is %d.\n", person1.age);
+    // person2->name = "Lisa";
     person2->age = 10;
-    printf("The person2 salary is %d.\n", person2->age);
+    printf("The person2 salary is %d.\n", (*person2).age);
+    // printf("Pointer to person2_ptr is %p\n", person2_ptr);
+    // free(person2_ptr);
 }
