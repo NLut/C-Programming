bd6a7f282c009f394d5070384fa1447176fe6860
NLut <Natapat2001@gmail.com>
2025-11-29 04:09:54 +0700

    Another bitwise operation

--- bitwise/bitwise.c
+++ bitwise/bitwise.c
@@ -1,6 +1,10 @@
+#include <stdint.h>
 #include <stdio.h>
 #include "bitwise.h"
 void binprintf(int v);
+void set(uint8_t *word, uint8_t b);
+uint8_t get(uint8_t *word, uint8_t b);
+void clear(uint8_t *word, uint8_t b);
 
 int main(){
     // Shift left bit by bit
@@ -36,6 +40,21 @@ int main(){
     printf("The value of 0 |& LONG2_S2 is: %ld, %08x\n", (x |= LONG2_S2), (x |= LONG2_S2)); // Bitwise or
     binprintf(x |= LONG2_S2);
 
+
+    puts("");
+    // bitwise or operation (set)
+    uint8_t word = 0x0;
+    uint8_t* word_pt = &word;
+    uint8_t bitLocation = 5;
+    printf("Set word value to: %x\n", word);
+    printf("Set bit %d\n", bitLocation);
+    set(word_pt, bitLocation);
+    printf("After set value: %x\n", word);
+    printf("Get bit %d\n", bitLocation);
+    printf("%d\n", get(word_pt, bitLocation));
+    printf("Clear bit %d\n", bitLocation);
+    clear(word_pt, bitLocation);
+    printf("After clear value: %x\n", word);
 }
 
 void binprintf(int v)
@@ -47,4 +66,21 @@ void binprintf(int v)
         mask >>= 1; // mask = mask shift right by 1 bit
     }
     puts("");
+}
+
+// bit wise set value
+
+void set(uint8_t *word, uint8_t b){
+    uint8_t mask = 1 << b; 
+    *word = *word | mask;
+}
+
+uint8_t get(uint8_t *word, uint8_t b){
+    uint8_t mask = 1;
+    return (*word >> b) & mask;
+}
+
+void clear(uint8_t *word, uint8_t b){
+    uint8_t mask = !(1 << b); // creat flipped mask: 0b00010000 -> 0b11101111 for b = 4
+    *word = *word & mask;
 }
