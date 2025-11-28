57582cec79cd8c7952f2aa37952c3bd17435e384
NLut <Natapat2001@gmail.com>
2025-11-29 04:57:21 +0700

    Add more concept

--- pointer/pointer.c
+++ pointer/pointer.c
@@ -3,12 +3,18 @@
 
 int main(){
     char* text = "Hello";
+    int a = 10;
+    int *a_ptr = &a; // pointer a_ptr store address of a by reference &a and dereference in by *
+    // * indicate that it's pointer
 
     printf("Address of string is: %p\n", &text);
     printf("Value of first char of string is: %c\n", *text);
     printf("Value of second char of string is: %c\n", *(text+1));
     printf("This is different definition: %c\n", *text+1);
     printf("Size of string is: %lu\n", strlen(text));
+    printf("The address of a is: %p\n", &a);
+    printf("The address of a is: %p\n", a_ptr); // a_ptr also hold the address value. 
+    printf("The dereference value of a is: %d\n", *a_ptr); // so we use * to do a dereference t get a value
     // int a = 4;
     // int* ptr = &a;
 
